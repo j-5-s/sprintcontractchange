0b15598463e1d06da27376a269b2d9bf57314560
James Charlesworth <james.charlesworth@gmail.com>
2011-12-03 16:24:25 -0500

    initial commit

---
+++ sprintcontractcheck.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -O http://shop2.sprint.com/en/legal/legal_terms_privacy_popup.shtml
+CMD='git status -s'
+export i=`$CMD`
+if ((${#i} > 0))
+then
+  `git commit -a -m "sprint contract change"`
+  `git push`
+fi
