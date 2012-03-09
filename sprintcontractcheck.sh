d1d5d510f67992422b29959e44937506e02b6281
james <james@james-tv.(none)>
2012-03-08 21:48:50 -0500

    sprint contract change

--- sprintcontractcheck.sh
+++ sprintcontractcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/james/src/sprintcontractchange/
+cd /home/james/apps/sprintcontractchange/
 curl -O http://shop2.sprint.com/en/legal/legal_terms_privacy_popup.shtml
 CMD='git status -s'
 export i=`$CMD`
